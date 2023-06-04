e8c62a7224ecb4bc684a948e329cba38e714842b
Yerson Felipe Ramirez Garnica <yfrg@gmail.com>
2023-06-04 16:38:03 -0500

    Desarrollo de responsive menu

---
+++ templates/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hackathon Taxis Libres 2023</title>
+</head>
+
+<body>
+    <header class="header__hackathon">
+        <img class="header__img" src="https://www.taxislibres.com.co/public/imagesNew/logo-escritorio.webp"
+            alt="Logo de taxis libres">
+        <section class="bars__menu--background">
+            <div class="bars__menu">
+                <span class="line1__bars-menu"></span>
+                <span class="line2__bars-menu"></span>
+                <span class="line3__bars-menu"></span>
+            </div>
+        </section>
+        <nav class="nav__mobile">
+            <ul class="nav__ul">
+                <li><a href="#">Nosotros <span class="material-symbols-outlined">expand_more</span></a></li>
+                <ul class="submenu__nosotros submenu__nav">
+                    <li><a href="#">Emisora Taxis Libres</a></li>
+                </ul>
+                <li><a href="#">Viajeros <span class="material-symbols-outlined">expand_more</span></a></li></a></li>
+                <ul class="submenu__viajeros submenu__nav">
+                    <li><a href="#">Descarga la App viajeros</a></li>
+                    <li><a href="#">Taxis por WhatsApp - Nacional</a></li>
+                </ul>
+                <li><a href="#">Empresas</a></li>
+                <li><a href="#">Vinculados <span class="material-symbols-outlined">expand_more</span></a></li></a></li>
+                <ul class="submenu__vinculados submenu__nav">
+                    <li><a href="#">Condutores</a></li>
+                    <li><a href="#">Propietaros</a></li>
+                    <li><a href="#">Beneficios</a></li>
+                    <li><a href="#">Mi perfil</a></li>
+                    <li><a href="#">Taxis disponibles</a></li>
+                </ul>
+                <li><a href="#">Ciudades <span class="material-symbols-outlined">expand_more</span></a></li></a></li>
+                <ul class="submenu__ciudades submenu__nav">
+                    <li><a href="#">Bogotá</a></li>
+                    <li><a href="#">Cali</a></li>
+                    <li><a href="#">Bucaramanga</a></li>
+                    <li><a href="#">Medellín</a></li>
+                    <li><a href="#">Cúcuta</a></li>
+                    <li><a href="#">Manizales</a></li>
+                </ul>
+                <li><a href="#">Más <span class="material-symbols-outlined">expand_more</span></a></li></a></li>
+                <ul class="submenu__mas submenu__nav">
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Trabajos disponibles</a></li>
+                    <li><a href="#">Sedes Bogotá</a></li>
+                    <li><a href="#">Centro de conocimiento</a></li>
+                    <li><a href="#">Postcast</a></li>
+                </ul>
+                <li><a href="#">Ayuda</a></li>
+            </ul>
+        </nav>
+    </header>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
