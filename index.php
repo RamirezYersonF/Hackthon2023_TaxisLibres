41f123ecce6f438d582be1f6f7ff055d9859e84b
Yerson Felipe Ramirez Garnica <yfrg@gmail.com>
2023-06-05 01:14:54 -0500

    informacion general del evento

--- index.php
+++ index.php
@@ -6,14 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700;800;900&family=Roboto:wght@300;400;500;700;900&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700;800;900&family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Hackathon Taxis Libres 2023</title>
 </head>
 
@@ -26,6 +21,31 @@
         </div>
         <section class="img_hackathon">
 
+        </section>
+    </section>
+    <section class="container__news">
+        <div class="container__size--news">
+            <h1 class="title__news">Noticias</h1>
+        </div>
+    </section>
+    <section class="container__descriptions-news">
+        <section class="container__title--descriptions">
+            <h2 class="information__descriptions">TaxiHack: Potenciando el Talento y la Creatividad en la Industria del
+                Transporte en Colombia</h2>
+        </section>
+    </section>
+    <section class="container__information--hackathon">
+        <section class="container__size--information">
+            <h2>Únete a nuestra Hackathon virtual de Taxis Libres y sé parte del impulso a la reactivación económica en
+                Colombia. En este evento, buscamos soluciones innovadoras para desafíos reales en el ámbito de la
+                movilidad en el país.</h2>
+            <h2>La Hackathon es completamente gratuita y abierta a equipos interdisciplinarios y personas apasionadas
+                por la innovación. Únete a una comunidad colaborativa y trabaja en equipo para desarrollar ideas que
+                transformen la industria de la movilidad.</h2>
+            <h2>Durante el evento, tendrás la oportunidad de trabajar en equipo, compartir conocimientos y aprovechar
+                las últimas tecnologías para abordar desafíos reales relacionados con la movilidad en el país. Además,
+                contarás con el apoyo de mentores y expertos de la industria para guiar tu desarrollo y enriquecer tu
+                experiencia.</h2>
         </section>
     </section>
     <br><br>
