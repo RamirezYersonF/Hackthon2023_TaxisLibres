6a065a2eb39b6b57bc95052be340b5df57c3a556
Yerson Felipe Ramirez Garnica <yfrg@gmail.com>
2023-06-07 04:07:59 -0500

    Listo todos los cambios en el front

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
+const menuMostrar = document.querySelector(".nav__mobile");
+const menuAbierto = document.querySelector(".bars__menu");
+function toggleMenu() {
+    menuMostrar.classList.toggle("activate__menu_js");
+};
+menuAbierto.addEventListener("click", toggleMenu);
+
+
 /*Home menu icon*/
-document.querySelector(".bars__menu").addEventListener("click", animateBars);
+document.querySelector(".bars__menu--background").addEventListener("click", animateBars);
 const line1__bars = document.querySelector(".line1__bars-menu");
 const line2__bars = document.querySelector(".line2__bars-menu");
 const line3__bars = document.querySelector(".line3__bars-menu");
@@ -9,6 +17,9 @@ function animateBars() {
     line2__bars.classList.toggle("activeline2__bars-menu");
     line3__bars.classList.toggle("activeline3__bars-menu");
 }
+
+
+
 /*Home menu icon*/
 /*Acordion*/
 const acc = document.getElementsByClassName("accordion");
